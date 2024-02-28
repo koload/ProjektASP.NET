d25cdbbbd70d0633825bf2a94cec24357dde5efa
Krzyś <Krzy@DESKTOP-Q97CREF>
2024-02-28 10:32:13 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace laby1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
